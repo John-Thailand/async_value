6d27f2bdd6d53efe9fb78faaeaa2711d2e43094f
John-Thailand <hamoban69@gmail.com>
2022-06-04 20:00:37 +0900

    AsyncValueWidgetの作成

--- lib/features/album/presentation/album_page.dart
+++ lib/features/album/presentation/album_page.dart
@@ -1,3 +1,4 @@
+import 'package:async_value/components/async_value_widget.dart';
 import 'package:async_value/features/album/data/album_repository.dart';
 import 'package:async_value/features/album/domain/album.dart';
 import 'package:flutter/material.dart';
@@ -8,15 +9,25 @@ class AlbumPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final AsyncValue<Album> album = ref.watch(albumProvider);
+    final AsyncValue<Album> albumValue = ref.watch(albumProvider);
 
+    // 基本的なAsyncValueの使い方
+    // return Scaffold(
+    //   body: albumValue.when(
+    //     loading: () => const Center(child: CircularProgressIndicator()),
+    //     error: (error, stack) =>
+    //         const Center(child: Text('Oops, something unexpected happened')),
+    //     data: (value) =>
+    //         Center(child: Text('${value.id} ${value.userId} ${value.title}')),
+    //   ),
+    // );
+
+    // Widgetに切り出した場合の使い方
     return Scaffold(
-      body: album.when(
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (error, stack) =>
-            const Center(child: Text('Oops, something unexpected happened')),
-        data: (value) =>
-            Center(child: Text('${value.id} ${value.userId} ${value.title}')),
+      body: AsyncValueWidget<Album>(
+        value: albumValue,
+        data: (album) =>
+            Center(child: Text('${album.id} ${album.userId} ${album.title}')),
       ),
     );
   }
